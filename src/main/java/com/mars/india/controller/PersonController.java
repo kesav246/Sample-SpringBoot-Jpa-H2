66615df95ff76316bb83380e51474f3580be3d0b
Kesav <venkatkesav24@gmail.com>
2021-05-30 23:30:11 +0530

    1. Log mechanism implemented 2. Documentation is implemented on method signature 3. Added executable jar plug-in property

--- src/main/java/com/mars/india/controller/PersonController.java
+++ src/main/java/com/mars/india/controller/PersonController.java
@@ -2,6 +2,8 @@ package com.mars.india.controller;
 
 import java.util.List;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -21,37 +23,83 @@ import com.mars.india.service.PersonService;
 @RestController
 @RequestMapping("/api")
 public class PersonController {
+	/**
+	* The Constant LOGGER.
+	*/
+	   private static final Logger LOGGER = LogManager.getLogger(PersonController.class);
 	
 	@Autowired
 	PersonService personService;
 	
+
+   /**
+    * this method is used to save Person content
+    *
+    * @param Person , not null
+    */
 	@PostMapping(value = "/savePerson", produces = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE }, consumes = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE })
 	public ResponseEntity<?> savePerson(@RequestBody Person person) {
+		LOGGER.trace("Entry :: savePerson in PersonController with params{}",
+				person);
 		return new ResponseEntity<>(personService.createPerson(person), HttpStatus.OK);
 	}
 	
+
+   /**
+    * this method is used to get all Persons content
+    */
 	@GetMapping("/allPersons")
     public ResponseEntity<List<Person>> getAllPersons() {
+		LOGGER.trace("Entry :: getAllPersons in PersonController");
         return new ResponseEntity<>(personService.getAllPersons(), HttpStatus.OK);
     }
 	
+
+   /**
+    * this method is used to update Person content by id
+    *
+    * @param id , not null
+    */
 	@PutMapping("/updatePerson/{id}")
 	public ResponseEntity<Person> update(@PathVariable int id, @RequestBody Person person)  {
+		LOGGER.trace("Entry :: update in PersonController with params{}", id);
         return new ResponseEntity<>(personService.update(id, person), HttpStatus.OK);
     }
+	
+
+   /**
+    * this method is used to get Person content by id
+    *
+    * @param id , not null
+    */
 
     @GetMapping("/person/{id}")
     public ResponseEntity<Person> getPersonById(@PathVariable Integer id) {
+    	LOGGER.trace("Entry :: getPersonById in PersonController with params{}", id);
         return new ResponseEntity<>(personService.getPersonById(id), HttpStatus.OK);
     }
+	
+
+    /**
+     * this method is used to delete Person content by id
+     *
+     * @param id , not null
+     */
 
     @DeleteMapping("/deletePerson/{id}")
     public boolean deletePerson(@PathVariable Integer id) {
+    	LOGGER.trace("Entry :: deletePerson in PersonController with params{}", id);
         return personService.deletePersonById(id);
     }
+	
+
+    /**
+     * this method is used to get count of Persons
+     */
     
     @GetMapping("/personsCount")
     public Long personsCount() {
+    	LOGGER.trace("Entry :: personsCount in PersonController");
         return personService.personsCount();
     }
 
--- src/main/java/com/mars/india/service/PersonService.java
+++ src/main/java/com/mars/india/service/PersonService.java
@@ -14,32 +14,58 @@ public class PersonService {
 	
 	@Autowired
 	PersonRepository personRepository;
-	
+
+   /**
+    * Returns Person
+    *
+    * @param Person , not null
+    */
 	public Person createPerson(Person person) {
 		return personRepository.save(person);
 	}
-	
+
+   /**
+    * Returns all persons list
+    */
     public List<Person> getAllPersons() {
         return (List<Person>) personRepository.findAll();
     }
 
+    /**
+     * Returns Person based on id
+     *
+     * @param id , not null
+     */
     public Person getPersonById(Integer id) {
         return personRepository.findById(id).orElseThrow(() -> new NotFoundException("Person not found for id :: " + id));
     }
 
+    /**
+     * Returns true/false based on person id
+     *
+     * @param id , not null
+     */
     public boolean deletePersonById(Integer id) {
     	personRepository.findById(id).ifPresentOrElse(Person -> personRepository.deleteById(id), 
     							() -> new NotFoundException("Person not found for id :: " + id));
 		return true;
     }
 
+    /**
+     * Returns person count
+     */
 	public Long personsCount() {
 		return personRepository.personsCount();
 	}
 
+	/**
+     * Returns person based on person id, person 
+     *
+     * @param id , not null
+     * @param Person
+     */
 	public Person update(Integer id, Person person) {
 		personRepository.findById(id).orElseThrow(() -> new NotFoundException("Person not found for id :: " + id));
-        
 		person.setId(id);
     	return personRepository.save(person);
 	}
